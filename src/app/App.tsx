b597cc67e891efb37b4afb3ef297693ca16dbf83
Roamer <jatgaret@gmail.com>
2020-06-26 23:50:32 +0300

    Добавил сплешку

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,17 +6,20 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import ToolBar from '../app/components/ToolBar/ToolBar';
 import NavigationController from './navigation/NavigationController/NavigationController';
 import EStyleSheet from 'react-native-extended-stylesheet';
-
+import SplashScreen from 'react-native-splash-screen';
 //тут будут все цвета для глобального доступа
 EStyleSheet.build({
     $toolbarColor:'#2C2C2C',
 });
 const App: () => React$Node = () => {
+    useEffect(()=>{
+        SplashScreen.hide();
+    });
   return (
     <NavigationContainer>
        <ToolBar/>
