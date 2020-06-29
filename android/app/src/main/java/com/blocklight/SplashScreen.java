9830762f5255462d2e2e808f3d7bb64070470624
Igor.Mikhailov <iomihailov@gmail.com>
2020-06-29 17:32:24 +0300

    fixed smth

--- android/app/src/main/java/com/blocklight/SplashScreen.java
+++ android/app/src/main/java/com/blocklight/SplashScreen.java
@@ -1,6 +1,5 @@
 package com.blocklight;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,31 +6,34 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import ToolBar from './components/ToolBar/ToolBar';
 import NavigationController from './navigation/NavigationController/NavigationController';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import SplashScreen from 'react-native-splash-screen';
-import {navigationRef,isReadyRef} from './navigation/RootNavigation/RootNavigation';
+import {
+  navigationRef,
+  isReadyRef,
+} from './navigation/RootNavigation/RootNavigation';
 import BottomTabBar from './components/BottomTabBar/BottomTabBar';
+
 //тут будут все цвета для глобального доступа
 EStyleSheet.build({
-    $toolbarColor:'#2C2C2C',
+  $toolbarColor: '#2C2C2C',
 });
 const App: () => React$Node = () => {
-    useEffect(()=>{
-        SplashScreen.hide();
-    });
+  useEffect(() => {
+    SplashScreen.hide();
+  });
 
-    return (
-        <NavigationContainer ref={navigationRef}>
-            <ToolBar/>
-            <NavigationController/>
-            <BottomTabBar/>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <ToolBar />
+      <NavigationController />
+      <BottomTabBar />
+    </NavigationContainer>
+  );
 };
-
 
 export default App;
